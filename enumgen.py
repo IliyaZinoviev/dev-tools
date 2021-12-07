2682306da8e759f87ce9489672b7ec8341d6f378
IliyaZinoviev <mrmar333@yandex.ru>
2021-12-07 22:32:01 +0300

    Add singularity noun conversion for list_field_type. Add enumgen.py.

---
+++ enumgen.py
@@ -0,0 +1,5 @@
+
+
+def gen_enum(input_items: [str]):
+    for item in set(input_items) :
+        print(f"\t{item} = '{item}'")
--- pydanticgen.py
+++ pydanticgen.py
@@ -2,11 +2,15 @@ from datetime import date, datetime
 from decimal import Decimal
 from typing import Union
 
+import inflect
+
 from helpers import call_multiple_assert
 from utils import is_camel, is_snake, convert2snake, convert2camel
 
 
-class PydanticModelsGenarator:
+class PydanticModelsGenerator:
+    inflect_engine: inflect.engine = inflect.engine()
+
     def __init__(self, input_data: dict = None, root_model_name: str = None, output_path_dir: str = None):
         call_multiple_assert(('input_data', input_data), ('root_model_name', root_model_name),
                              ('output_path_dir', output_path_dir))
@@ -15,9 +19,11 @@ class PydanticModelsGenarator:
         self.root_model_name: str = root_model_name
         self.output_path_dir: str = output_path_dir
 
-    def _get_field_type(self, val: Union[dict, str, int, float, bool], alias: str):
+    def _get_field_type(self, val: Union[dict, str, int, float, bool], alias: str, is_seq_element: bool = False):
         if isinstance(val, dict):
-            res = self._generate_pydantic_model_hierarchy(val, alias)
+            res = self._generate_pydantic_model_hierarchy(val, alias, is_seq_element)
+        elif isinstance(val, bool):
+            res = 'bool'
         elif isinstance(val, str):
             res = 'str'
         elif isinstance(val, int):
@@ -30,15 +36,14 @@ class PydanticModelsGenarator:
             res = 'datetime'
         elif isinstance(val, date):
             res = 'datetime'
-        elif isinstance(val, bool):
-            res = 'bool'
         else:
             res = 'Any'
         return res
 
-    def _generate_pydantic_model_hierarchy(self, input_data: dict, class_name: str):
+    def _generate_pydantic_model_hierarchy(self, input_data: dict, class_name: str, is_seq_element: bool = False):
         #  todo investigate how to add Optional
         parent_model: str
+        class_name_issue_comment: str = ''
         if 'id' in input_data and isinstance(input_data['id'], int):
             input_data.pop('id')
             parent_model = 'IdSchema'
@@ -52,13 +57,17 @@ class PydanticModelsGenarator:
             class_name = ''.join([first.title()] + others)
         else:
             class_name = class_name
-        header = f'class {class_name}({parent_model}):\n'
+        if is_seq_element:
+            if singular_noun := self.inflect_engine.singular_noun(class_name):
+                class_name = singular_noun
+            class_name_issue_comment = '  # todo check/select correct singular noun.'
+        header = f'class {class_name}({parent_model}):{class_name_issue_comment}\n'
         if class_name not in self.models:
             self.models[class_name] = [header]
             for alias, val in input_data.items():
                 if isinstance(val, list):
                     list_val = val[0]
-                    list_field_type = self._get_field_type(list_val, alias)
+                    list_field_type = self._get_field_type(list_val, alias, True)
                     field_type = f'list[{list_field_type}]'
                 else:
                     field_type = self._get_field_type(val, alias)
@@ -79,8 +88,9 @@ class PydanticModelsGenarator:
             for model in list(self.models.values())[::-1]:
                 file.writelines(model)
 
+
 if __name__ == '__main__':
     input_data = {}
     root_model_name = 'ClassName'
     output_path_dir = ''
-    PydanticModelsGenarator(input_data, root_model_name, output_path_dir).exec()
+    PydanticModelsGenerator(input_data, root_model_name, output_path_dir).exec()
